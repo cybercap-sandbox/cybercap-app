6eaef4ff2b4d86d7237bf195fa7078e45f6edd45
Aleksandr Efimenko <mail@alexefimenko.com>
2023-09-20 15:44:38 +0400

    Disable saving images urls related to open ai server because they are available only for 1 hour

--- src/pages/image-generation/index.tsx
+++ src/pages/image-generation/index.tsx
@@ -2,12 +2,11 @@ import Head from "next/head";
 import { ImageGenerationPromptForm } from "@/components/openai-playground/image-promp-form";
 import { Layout } from "@/components/layout";
 import { api } from "@/utils/api";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import type * as z from "zod";
 import { type imgGenFormSchema } from "@/components/openai-playground/image-promp-form";
 import { ImgGallery } from "@/components/openai-playground/image-gallery";
-import { useSession } from "next-auth/react";
-import { Icons } from "@/components/icons";
+import { useSaveImageRequest } from "@/hooks/saveImageRequest";
 
 export type GenerateImageParams = {
   prompt: string;
@@ -15,63 +14,26 @@ export type GenerateImageParams = {
   size: "256x256" | "512x512" | "1024x1024";
 };
 export default function Home() {
-  const { data: session } = useSession();
   const [imgGenStatus, setImgGenStatus] = useState<
     "idle" | "pending" | "fulfilled" | "rejected"
   >("idle");
   const [generatedImages, setGeneratedImages] = useState<
     (string | undefined)[]
   >([]);
-  const [userRequestId, setUserRequestId] = useState<string | undefined>(
-    undefined
-  );
-
-  const saveUserRequestMutation =
-    api.imageGenerationLog.saveUserRequest.useMutation();
-  const saveGeneratedImagesMutation =
-    api.imageGenerationLog.saveGeneratedImages.useMutation();
-
-  const getGeneratedImagesQuery =
-    api.imageGenerationLog.getAllImagesGeneratedByUser.useQuery(
-      { numberOfImages: 10 },
-      {
-        enabled: !!session,
-      }
-    );
-
-  useEffect(() => {
-    if (!getGeneratedImagesQuery.data) return;
-    const requests = getGeneratedImagesQuery.data?.map(
-      (d) => d.generatedImages
-    );
-    if (!requests) return;
-    const images = requests.flat().map((d) => d.imageUrl);
-    setGeneratedImages(images);
-  }, [getGeneratedImagesQuery.data]);
+  const { saveUserRequest, isMutationLoading } = useSaveImageRequest();
 
   const generateImageMutation = api.openai.generateImage.useMutation({
     onMutate: () => {
-      // console.log("pending");
       setImgGenStatus("pending");
     },
     onError: () => {
-      // console.log(error);
       setImgGenStatus("rejected");
     },
     onSuccess: (data) => {
-      // console.log("success");
       if (!data.response) return;
       setImgGenStatus("fulfilled");
       const imgUrls = data.response?.map((d) => d.url!);
-      if (!imgUrls) return;
       setGeneratedImages((prev) => [...imgUrls, ...prev]);
-      // save generated images urls to db
-      if (userRequestId) {
-        saveGeneratedImagesMutation.mutate({
-          imageGenerationRequestId: userRequestId,
-          generatedImages: imgUrls,
-        });
-      }
     },
   });
 
@@ -88,22 +50,16 @@ export default function Home() {
     });
   };
   const submitHandler = async (values: z.infer<typeof imgGenFormSchema>) => {
-    //save user request and get id
-    const userRequest = await saveUserRequestMutation.mutateAsync({
-      requestText: values.prompt,
-      imageSize: values.size,
-      numberOfImages: values.n,
-    });
-    setUserRequestId(userRequest.id);
     generateImg({
       prompt: values.prompt,
       numberOfImages: values.n,
       size: values.size,
     });
+    //save user request and get id
+    await saveUserRequest({ values });
   };
-  const isLoading =
-    imgGenStatus === "pending" ||
-    (session && getGeneratedImagesQuery.isLoading);
+
+  const isMutating = imgGenStatus === "pending" || isMutationLoading;
   return (
     <>
       <Head>
@@ -117,19 +73,9 @@ export default function Home() {
             <ImageGenerationPromptForm
               // eslint-disable-next-line @typescript-eslint/no-misused-promises
               submitHandler={submitHandler}
-              isLoading={imgGenStatus === "pending"}
+              isLoading={isMutating}
             />
-            <div>
-              {isLoading && (
-                <div className="flex h-full w-full items-center justify-center">
-                  <Icons.spinner
-                    className="h-10 w-10 animate-spin"
-                    fill="black"
-                  />
-                </div>
-              )}
-              <ImgGallery images={generatedImages as string[]} />
-            </div>
+            <ImgGallery images={generatedImages as string[]} />
           </div>
         </main>
       </Layout>
