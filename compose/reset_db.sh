cc5fbf167dd34a4e9e17cbd55b368573fd4b9034
Aleksandr Efimenko <mail@alexefimenko.com>
2023-09-29 19:11:13 +0400

    rebuild web docker image before it will be able to use the Prisma schema

--- compose/reset_db.sh
+++ compose/reset_db.sh
@@ -2,7 +2,7 @@ docker-compose stop db
 docker-compose rm -f db
 docker-compose up -d db
 sleep 15
-
-docker-compose exec -t web #command to migrate using prisma
+docker-compose rm -f web #remove web container to force rebuild
+docker-compose build --parallel #rebuild web container
 docker-compose exec -t web npx prisma db push #command to migrate using prisma
 docker-compose exec -t web npx prisma db seed #command to add seed data using prisma
