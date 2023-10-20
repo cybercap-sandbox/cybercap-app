addb0d09b53fefa8162a8743089693fa4e55773c
Aleksandr Efimenko <mail@alexefimenko.com>
2023-10-20 18:36:44 +0400

    check user data does not exists if user is not authorized

--- cypress/e2e/mainPage.cy.ts
+++ cypress/e2e/mainPage.cy.ts
@@ -6,21 +6,18 @@ export type TestUser = {
   sessionToken: string;
 };
 
-describe("Checks main page content", () => {
-  beforeEach(() => {
-    cy.visit("/");
-  });
-});
-
 describe("Test authorized user", function () {
-  beforeEach(function () {
-    cy.loginWithMockUser();
-  });
-
   it("Shows user name and email on the main page", function () {
+    cy.loginWithMockUser();
     cy.fixture<TestUser>("testUser").then((user) => {
       cy.dataCy("welcomeName").should("contain.text", user.name);
       cy.dataCy("welcomeEmail").should("contain.text", user.email);
     });
+  });
+
+  it("Should not show user data if the user is not authorized", function () {
+    cy.clearCookie("next-auth.session-token");
+    cy.dataCy("welcomeName").should("not.exist");
+    cy.dataCy("welcomeEmail").should("not.exist");
   });
 });
