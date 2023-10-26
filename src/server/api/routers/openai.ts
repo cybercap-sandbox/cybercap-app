8f0b23b5684db0ed6c4dc05f6a72ca38a208c570
Aleksandr Efimenko <mail@alexefimenko.com>
2023-10-26 14:22:59 +0400

    remove commented code

--- src/server/api/routers/openai.ts
+++ src/server/api/routers/openai.ts
@@ -10,32 +10,6 @@ export const openAiRouter = createTRPCRouter({
     const list = await openai.models.list();
     return list.data.map((model) => model.id);
   }),
-  // generateCompletion: publicProcedure
-  //   .input(
-  //     z.object({
-  //       model: z.string(),
-  //       prompt: z.string(),
-  //       max_tokens: z.number().optional(),
-  //       temperature: z.number().optional(),
-  //     })
-  //   )
-  //   .mutation(async ({ input }) => {
-  //     console.log(input);
-  //     try {
-  //       const response = await openai.chat.completions.create({
-  //         model: input.model,
-  //         messages: [{ role: "user", content: input.prompt }],
-  //         temperature: input.temperature ? input.temperature : 0.9,
-  //         max_tokens: input.max_tokens ? input.max_tokens : 150,
-  //         stream: true,
-  //       });
-
-  //       const stream = OpenAIStream(response);
-  //       return new StreamingTextResponse(stream);
-  //     } catch (error) {
-  //       console.log(error);
-  //     }
-  //   }),
 
   generateImage: publicProcedure
     .input(
