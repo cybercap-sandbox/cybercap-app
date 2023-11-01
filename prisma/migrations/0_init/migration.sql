f487f4f1e384e23d1d4772142b39aae8892f9896
Aleksandr Efimenko <mail@alexefimenko.com>
2023-11-01 23:14:57 +0400

    add initial data in migration file

--- prisma/migrations/0_init/migration.sql
+++ prisma/migrations/0_init/migration.sql
@@ -148,3 +148,23 @@ ALTER TABLE "ChatMessage" ADD CONSTRAINT "ChatMessage_modelName_fkey" FOREIGN KE
 -- AddForeignKey
 ALTER TABLE "ChatSession" ADD CONSTRAINT "ChatSession_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
 
+-- PopulateTable
+-- For gpt-4
+INSERT INTO "OpenAIModels" ("name", "availableForChat", "availableForEmbeddings", "availableForAudioTranscript", "availableForFineTuningJobs", "availableForFineTunes", "description", "maxTokens", "TrainingDataDate")
+VALUES ('gpt-4', true, false, false, false, false, 'More capable than any GPT-3.5 model, able to do more complex tasks, and optimized for chat. Will be updated with our latest model iteration 2 weeks after it is released.', 8192, '2021-09-01');
+
+-- For gpt-4-0613
+INSERT INTO "OpenAIModels" ("name", "availableForChat", "availableForEmbeddings", "availableForAudioTranscript", "availableForFineTuningJobs", "availableForFineTunes", "description", "maxTokens", "TrainingDataDate")
+VALUES ('gpt-4-0613', true, false, false, false, false, 'Snapshot of gpt-4 from June 13th 2023 with function calling data. Unlike gpt-4, this model will not receive updates, and will be deprecated 3 months after a new version is released.', 8192, '2021-09-01');
+
+-- For gpt-3.5-turbo
+INSERT INTO "OpenAIModels" ("name", "availableForChat", "availableForEmbeddings", "availableForAudioTranscript", "availableForFineTuningJobs", "availableForFineTunes", "description", "maxTokens", "TrainingDataDate")
+VALUES ('gpt-3.5-turbo', true, false, false, true, false, 'Most capable GPT-3.5 model and optimized for chat at 1/10th the cost of text-davinci-003. Will be updated with our latest model iteration 2 weeks after it is released.', 4097, '2021-09-01');
+
+-- For gpt-3.5-turbo-16k
+INSERT INTO "OpenAIModels" ("name", "availableForChat", "availableForEmbeddings", "availableForAudioTranscript", "availableForFineTuningJobs", "availableForFineTunes", "description", "maxTokens", "TrainingDataDate")
+VALUES ('gpt-3.5-turbo-16k', true, false, false, false, false, 'Same capabilities as the standard gpt-3.5-turbo model but with 4 times the context.', 16384, '2021-09-01');
+
+-- For gpt-3.5-turbo-0613
+INSERT INTO "OpenAIModels" ("name", "availableForChat", "availableForEmbeddings", "availableForAudioTranscript", "availableForFineTuningJobs", "availableForFineTunes", "description", "maxTokens", "TrainingDataDate")
+VALUES ('gpt-3.5-turbo-0613', true, false, false, false, false, 'Snapshot of gpt-3.5-turbo from June 13th 2023 with function calling data. Unlike gpt-3.5-turbo, this model will not receive updates, and will be deprecated 3 months after a new version is released.', 4097, '2021-09-01');
